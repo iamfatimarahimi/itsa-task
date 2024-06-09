bb008981731ecc3a656bdb4d4d8bc5ad6ecce4f9
fatima rahimi <iamfatimarahimi@gmail.com>
2024-06-09 20:39:28 +0330

    adding slide menu

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import './App.css';
-import './script.js';
 import Swiper from'./Swiper.js';
+import SlideMenu from './SlideMenu.js'
 //Import Images
-import logoPic from './images/logo.png';
+// import logoPic from './images/logo.png';
  export default function App() {
   return (
     <div classNameName="App">
           <div className="container">
       <header>
-        <div className="logo">
-          <img src={logoPic} alt="Logo"/>
-        </div>
-        <ul className="navigation">
-          <li><a href="#">دسته بندی</a></li>
-          <li><a href="AboutMe.html">راهنمای خرید</a></li>
-          <li><a href="./form/login.html">درباره ما</a></li>
-        </ul>
-        <div className="search">
-          <input type="text" placeholder="Search" />
-          <i className="bx bx-search-alt-2"></i>
-        </div>
+        <SlideMenu />
       </header>
       <section className="slider">
-        <Swiper />
+        {/* <Swiper /> */}
+        {/* <div className="search">
+          <input type="text" placeholder="Search" />
+          <i className="bx bx-search-alt-2"></i>
+        </div> */}
       </section>
     </div>
     </div>
--- src/Swiper.js
+++ src/Swiper.js
@@ -5,32 +5,36 @@ import './App.css';
 // Import Swiper styles
 import 'swiper/css';
 import { Autoplay } from 'swiper/modules';
-import 'swiper/css/Autoplay ';
+// import 'swiper/css/Autoplay ';
 //Import images
-
 import slide1 from './images/computer background.jpg';
 import slide2 from './images/evening-landscape-minimal-4k-kl.jpg';
 import slide3 from './images/10.jpg';
 import slide4 from './images/Slide4.png';
+
+
 export default () => {
   return (
     <Swiper
-    modules={[Autoplay]}
+      modules={[Autoplay]}
       spaceBetween={50}
       slidesPerView={3}
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
       loop={true}
-                autoplay={{
-                    delay: 500,
+      autoplay={{
+                    delay: 1500,
                     disableOnInteraction: false
                 }}
     >
-      <SwiperSlide><img src={slide1}  alt="slide1"/></SwiperSlide>
-      <SwiperSlide><img src={slide2}  alt="slide1"/></SwiperSlide>
-      <SwiperSlide><img src={slide3}  alt="slide1"/></SwiperSlide>
-      <SwiperSlide><img src={slide4}  alt="slide1"/></SwiperSlide>
-      ...
+      
+
+      <div className="swiper">
+              <SwiperSlide><img className='slideImage' src={slide1}  alt="slide1"/></SwiperSlide>
+              <SwiperSlide><img className='slideImage' src={slide2}  alt="slide2"/></SwiperSlide>
+              <SwiperSlide><img className='slideImage' src={slide3}  alt="slide3"/></SwiperSlide>
+              <SwiperSlide><img className='slideImage' src={slide4}  alt="slide4"/></SwiperSlide>
+        </div>
     </Swiper>
   );
 };
